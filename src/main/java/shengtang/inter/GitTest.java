a1e87084517bb7ea16a23043307ded3f61060346
firefly <524623291@qq.com>
2023-06-17 09:30:20 +0800

    idea third commit

--- src/main/java/shengtang/inter/GitTest.java
+++ src/main/java/shengtang/inter/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git,hello java!");
         System.out.println("hello git2,hello java2!");
+        System.out.println("hello git3,hello java!3");
     }
 }
